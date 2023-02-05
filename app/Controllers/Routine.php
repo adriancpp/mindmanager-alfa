cde312393b13e9068e62b9f769cdb7ccb947a177
adriancpp <adrian.cpp@wp.pl>
2023-02-05 23:16:40 +0100

    almost finished new routine view

--- app/Controllers/Routine.php
+++ app/Controllers/Routine.php
@@ -17,6 +17,23 @@ class Routine extends BaseController
     {
         $data = [];
 
+        $data['types'] = [
+            ['YESNO', 'Tak/Nie', true],
+            ['TIME', 'Czas', false],
+            ['COUNT', 'Ilość', false]
+        ];
+
+        $data['prioritys'] = [
+            [1, 'Niski', false],
+            [2, 'Średni', true],
+            [3, 'Wysoki', false]
+        ];
+
+        $data['actives'] = [
+            [1, 'Aktywny', true],
+            [0, 'Dezaktywowany', false]
+        ];
+
         echo view('templates/header', $data);
         echo view('newRoutine', $data);
         echo view('templates/footer', $data);
--- app/Views/newRoutine.php
+++ app/Views/newRoutine.php
@@ -6,18 +6,57 @@
                 <hr>
                 <form class="" action="/routine/new" method="post">
                     <div class="row">
-                        <div class="col-12 col-sm-6">
+
+                        <div class="col-12">
                             <div class="form-group">
                                 <label for="name">Nazwa</label>
                                 <input type="text" class="form-control" name="name" id="name" value="">
                             </div>
                         </div>
-                        <div class="col-12">
+                        <div class="col-12 col-sm-6">
                             <div class="form-group">
-                                <label for="email">Adres e-mail</label>
-                                <input type="text" class="form-control" name="email" id="email" value="">
+                                <label for="type">Sposób zaliczenia</label>
+                                <select name="type" class="form-control" onchange="checkIfShowRequiredAmount(this);">
+                                    <?php foreach ($types as $type): ?>
+                                        <option <?php if($type[2]==true) echo ' selected ' ?> value="<?= $type[0] ?>"><?= $type[1] ?></option>
+                                    <?php endforeach; ?>
+                                </select>
                             </div>
                         </div>
+                        <div class="col-12 col-sm-6">
+                            <div class="form-group" id="required_amount_div" style="display: none;">
+                                <label for="required_amount">Próg zaliczenia</label>
+                                <input type="text" class="form-control" name="required_amount" id="required_amount" value="">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <div class="form-group">
+                                <label for="priority">Priorytet</label>
+                                <select name="priority" class="form-control">
+                                    <?php foreach ($prioritys as $priority): ?>
+                                        <option <?php if($priority[2]==true) echo ' selected ' ?> value="<?= $priority[0] ?>"><?= $priority[1] ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <div class="form-group">
+                                <label for="sort">Kolejność</label>
+                                <input type="number" class="form-control" name="sort" id="sort" value="">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <div class="form-group">
+                                <label for="active">Stan</label>
+                                <select name="active" class="form-control">
+                                    <?php foreach ($actives as $active): ?>
+                                        <option <?php if($active[2]==true) echo ' selected ' ?> value="<?= $active[0] ?>"><?= $active[1] ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+
+
                     </div>
 
                     <?php if(isset($validation)): ?>
@@ -27,8 +66,22 @@
                             </div>
                         </div>
                     <?php endif; ?>
+                    <div class="row">
+                        <div class="col-12 col-sm-4">
+                            <button type="submit" class="btn btn-primary">Zapisz</button>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
+<script>
+    function checkIfShowRequiredAmount(that) {
+        if (that.value != "YESNO") {
+            document.getElementById("required_amount_div").style.display = "block";
+        } else {
+            document.getElementById("required_amount_div").style.display = "none";
+        }
+    }
+</script>
