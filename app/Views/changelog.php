18bf991e25e32b66726290e6384710b303f656ee
adriancpp <adrian.cpp@wp.pl>
2023-02-06 22:31:21 +0100

    prod update

--- app/Views/changelog.php
+++ app/Views/changelog.php
@@ -2,6 +2,22 @@
 
     <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 mt-5 pt-3 pb-3 bg-white from-wrapper">
+            <h4>6.02.2023</h4>
+            <p>
+            <p>Rutyny</p>
+            <p>- wstępna wersja dodawania</p>
+            <p>- lista rutyn</p>
+            <p></p>
+            <p></p>
+            <p>W przygotowaniu:</p>
+            <p>- edycja rutyn</p>
+            <p><font style="color: blue"> - funkcja odhaczania codziennych rutyn!</font></p>
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 mt-5 pt-3 pb-3 bg-white from-wrapper">
             <h4>3.02.2023</h4>
             <p>
                 - naprawione menu na małych wyświetlaczach
--- db.sql
+++ db.sql
@@ -1,1 +1,15 @@
 CREATE TABLE `mind_manager`.`user` (`id` INT NOT NULL AUTO_INCREMENT , `login` VARCHAR(20) NOT NULL , `nickname` VARCHAR(20) NOT NULL , `email` VARCHAR(50) NOT NULL , `password` VARCHAR(255) NOT NULL , `created_at` DATETIME NOT NULL , `updated_at` DATETIME NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+
+CREATE TABLE `routine` (
+                           `id` int NOT NULL,
+                           `user_id` int NOT NULL,
+                           `name` varchar(50) COLLATE utf8mb4_polish_ci NOT NULL,
+                           `type` varchar(20) COLLATE utf8mb4_polish_ci NOT NULL,
+                           `created_at` datetime NOT NULL,
+                           `updated_at` datetime NOT NULL,
+                           `sort` int NOT NULL,
+                           `priority` int NOT NULL,
+                           `required_amount` int DEFAULT NULL,
+                           `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
