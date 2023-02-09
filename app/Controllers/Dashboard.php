aa6a321d92ca3e5cb7c4e8606dea3f03378a75e9
adriancpp <adrian.cpp@wp.pl>
2023-02-09 19:30:55 +0100

    ongoin task dashboard

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2,11 +2,21 @@
 
 namespace App\Controllers;
 
+use App\Models\RoutineRepository;
+
 class Dashboard extends BaseController
 {
     public function index()
     {
         $data = [];
+
+        $db = db_connect();
+        $model = new RoutineRepository($db);
+
+        $result = $model->all(session()->get('id'));
+        // ->getUserRoutinesDashboard()    -> where day in join routine history if true -> status = ok, else 'no'
+
+        $data['routines'] = $result;
 
         echo view('templates/header', $data);
         echo view('dashboard', $data);
