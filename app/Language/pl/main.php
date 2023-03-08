d410c3bc5b2e950478e077aef2aa11ae5486b042
adriancpp <adrian.cpp@wp.pl>
2023-03-08 22:43:41 +0100

    main lang

---
+++ app/Language/pl/main.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+'title' => "Mind Manager",
+'nav_mainPage' => "Strona główna",
+'metaKeywords' => "CodeIgniter, language, example",
+'languageName_de' => "German (Deutsch)",
+'languageName_en' => "English",
+'languageName_es' => "Spanish (Español)",
+'languageName_fr' => "French (Français)",
+'languageName_ja' => "Japanese (日本語)",
+'aboutTitle' => "About",
+'aboutText' => "Hello, here is an example of a multilingual blog.",
+];
