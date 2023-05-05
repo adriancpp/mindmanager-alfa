1d7a4339fef74441a3b6e1c1652fe3168b9f66d2
adriancpp <adrian.cpp@wp.pl>
2023-05-05 15:22:16 +0200

    some idea of sound alert

--- public/adrianxk/hours/index.php
+++ public/adrianxk/hours/index.php
@@ -2,6 +2,13 @@
 
 //function setStoperAt( $actualTime, $mode )
 // alert() <-- play song until click OK
+<audio id="xyz" src="whatever_you_want.mp3" preload="auto"></audio>
+
+if(x > 10)
+{
+    document.getElementById('xyz').play();
+    alert("Thank you!");
+}
 
     if(isset($_POST['start']))
     {
