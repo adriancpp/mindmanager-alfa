7281b40dcc3a6f93497c80a694dc775427b21038
adriancpp <adrian.cpp@wp.pl>
2024-01-14 11:53:58 +0100

    nedd PoC

--- app/Controllers/Planner.php
+++ app/Controllers/Planner.php
@@ -8,6 +8,20 @@ use App\Models\UserFriendModel;
 use App\Models\UserModel;
 use App\Repository\UserFriendRepository;
 
+/*
+    Idea:
+1. pola w bazie:
+- id taska
+- nazwa
+- kiedy ostatnio robilem
+- jak czesto powinienem robic
+0. hmm mozna uzyc juz tego co mam - od poczatku juz mialem przeciez...
+
+Jak to w ogole ma dzialac?
+
+
+*/
+
 class Planner extends BaseController
 {
     public static function index()
